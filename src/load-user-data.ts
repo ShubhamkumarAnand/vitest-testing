55618c8abec9139b8613aba40f8ebd92126c99ac
imskanand <the.anand3099@gmail.com>
2023-02-10 18:19:53 +0530

    feat: Add favorite Food

--- src/load-user-data.ts
+++ src/load-user-data.ts
@@ -1,32 +1,35 @@
 interface User {
-  username: string,
-  name: string,
-  projects: string[],
-  coolness?: number
+	username: string;
+	name: string;
+	projects: string[];
+	coolness?: number;
+	favoriteFood: string;
 }
 
 async function loadUser(username: string): Promise<User | undefined> {
-  const users: User[] = [
-    {
-      username: 'imskanand',
-      name: 'shubham',
-      projects: ['buyzon', 'hindu-way']
-    },
-    {
-      username: 'satyamjee',
-      name: 'satyam',
-      projects: ['love', 'laugh']
-    }
-  ]
+	const users: User[] = [
+		{
+			username: 'imskanand',
+			name: 'shubham',
+			projects: ['buyzon', 'hindu-way'],
+			favoriteFood: 'litti-chokha',
+		},
+		{
+			username: 'satyamjee',
+			name: 'satyam',
+			projects: ['love', 'laugh'],
+			favoriteFood: 'litti-chokha',
+		},
+	];
 
-  return users.find((user) => user.username === username)
+	return users.find((user) => user.username === username);
 }
 
 export async function loadUserData(username: string) {
-  const user = await loadUser(username)
-  if (!user) {
-    throw new Error('No User Found');
-  }
-  user.coolness = username === 'imskanand' ? 100 : -1;
-  return user
+	const user = await loadUser(username);
+	if (!user) {
+		throw new Error('No User Found');
+	}
+	user.coolness = username === 'imskanand' ? 100 : -1;
+	return user;
 }
