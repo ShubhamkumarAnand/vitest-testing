0eceaa49d0dee8107e569f3c9f13cf47ab346050
imskanand <the.anand3099@gmail.com>
2023-02-10 18:16:19 +0530

    feat: working snapshot test

--- src/load-user-data.test.ts
+++ src/load-user-data.test.ts
@@ -1,27 +1,21 @@
-import { loadUserData } from './load-user-data'
-import { describe, test, expect } from 'vitest'
+import { loadUserData } from './load-user-data';
+import { describe, test, expect } from 'vitest';
 
 describe('loadUserData', () => {
-  test('load user data as expected', async () => {
-    const user = await loadUserData('imskanand')
-    expect(user).toEqual({
-      coolness: 100,
-      username: 'imskanand',
-      name: 'shubham',
-      projects: ['buyzon', 'hindu-way']
-    })
-  })
+	test('load user data as expected', async () => {
+		const user = await loadUserData('imskanand');
+		expect(user).toMatchSnapshot();
+	});
 
-  test('Setting up the coolness correctly', async () => {
-    const imskanand = await loadUserData('imskanand')
-    const satyam = await loadUserData('satyamjee')
+	test('Setting up the coolness correctly', async () => {
+		const imskanand = await loadUserData('imskanand');
+		const satyam = await loadUserData('satyamjee');
 
-    expect(imskanand.coolness).toBe(100)
-    expect(satyam.coolness).toBe(-1)
+		expect(imskanand.coolness).toBe(100);
+		expect(satyam.coolness).toBe(-1);
+	});
 
-  })
-
-  test('Throws the error which does not exists', async () => {
-    expect(async () => await loadUserData('fakeuser')).rejects.toThrowError('No User Found')
-  })
-})
+	test('Throws the error which does not exists', async () => {
+		expect(async () => await loadUserData('fakeuser')).rejects.toThrowError('No User Found');
+	});
+});
